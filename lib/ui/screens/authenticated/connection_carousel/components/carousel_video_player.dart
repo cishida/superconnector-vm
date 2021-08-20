36fe4e6ad7c5c6e0e60a83e024c943a7aecdfcd4
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2021-08-19 18:43:49 -0700

    Cleaning up video player.

--- lib/ui/screens/authenticated/connection_carousel/components/carousel_video_player.dart
+++ lib/ui/screens/authenticated/connection_carousel/components/carousel_video_player.dart
@@ -22,95 +22,14 @@ class CarouselVideoPlayer extends StatefulWidget {
 }
 
 class _CarouselVideoPlayerState extends State<CarouselVideoPlayer> {
-  // late BetterPlayerController _betterPlayerController;
-  // late BetterPlayerDataSource _betterPlayerDataSource;
   Superuser? _superuser;
   Duration? _duration = Duration(seconds: 0);
   Duration? _position = Duration(seconds: 0);
   VideoPlayerHelper _videoPlayerHelper = VideoPlayerHelper();
   BetterPlayerTheme _playerTheme = BetterPlayerTheme.custom;
-
-  // @override
-  // void initState() {
-  //   BetterPlayerConfiguration betterPlayerConfiguration =
-  //       BetterPlayerConfiguration(
-  //     aspectRatio: 16 / 9,
-  //     fit: BoxFit.contain,
-  //     autoPlay: true,
-  //   );
-  //   _betterPlayerDataSource = BetterPlayerDataSource(
-  //     BetterPlayerDataSourceType.network,
-  //     'https://stream.mux.com/' + widget.video.playbackIds[0] + '.m3u8',
-  //     cacheConfiguration: BetterPlayerCacheConfiguration(
-  //       useCache: true,
-  //       preCacheSize: 10 * 1024 * 1024,
-  //       maxCacheSize: 10 * 1024 * 1024,
-  //       maxCacheFileSize: 10 * 1024 * 1024,
-
-  //       ///Android only option to use cached video between app sessions
-  //       key: "testCacheKey",
-  //     ),
-  //   );
-  //   _betterPlayerController = BetterPlayerController(betterPlayerConfiguration);
-  //   super.initState();
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text("Cache"),
-  //     ),
-  //     body: Column(
-  //       children: [
-  //         const SizedBox(height: 8),
-  //         Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 16),
-  //           child: Text(
-  //             "Player with cache enabled. To test this feature, first plays "
-  //             "video, then leave this page, turn internet off and enter "
-  //             "page again. You should be able to play video without "
-  //             "internet connection.",
-  //             style: TextStyle(fontSize: 16),
-  //           ),
-  //         ),
-  //         AspectRatio(
-  //           aspectRatio: 16 / 9,
-  //           child: BetterPlayer(controller: _betterPlayerController),
-  //         ),
-  //         TextButton(
-  //           child: Text("Start pre cache"),
-  //           onPressed: () {
-  //             _betterPlayerController.preCache(_betterPlayerDataSource);
-  //           },
-  //         ),
-  //         TextButton(
-  //           child: Text("Stop pre cache"),
-  //           onPressed: () {
-  //             _betterPlayerController.stopPreCache(_betterPlayerDataSource);
-  //           },
-  //         ),
-  //         TextButton(
-  //           child: Text("Play video"),
-  //           onPressed: () {
-  //             _betterPlayerController.setupDataSource(_betterPlayerDataSource);
-  //           },
-  //         ),
-  //         TextButton(
-  //           child: Text("Clear cache"),
-  //           onPressed: () {
-  //             _betterPlayerController.clearCache();
-  //           },
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
   late BetterPlayerController _betterPlayerController;
 
   void _initializeVideo() async {
-    // Size size = MediaQuery.of(context).size;
-
     BetterPlayerDataSource betterPlayerDataSource = BetterPlayerDataSource(
       BetterPlayerDataSourceType.network,
       'https://stream.mux.com/' + widget.video.playbackIds[0] + '.m3u8',
@@ -156,37 +75,6 @@ class _CarouselVideoPlayerState extends State<CarouselVideoPlayer> {
         });
       }
     });
-
-    // _betterPlayerController.addEventsListener((event) {
-    //   print("Better player event: ${event.betterPlayerEventType}");
-    //   if (mounted) {
-    //     setState(() {});
-    //   }
-    // });
-
-    // _videoPlayerListener = () {
-    //   print('here');
-    //   if (mounted) {
-    //     setState(() {});
-    //   }
-    // };
-
-    // if (_betterPlayerController.videoPlayerController != null) {
-    //   _betterPlayerController.videoPlayerController!.addListener(
-    //     _videoPlayerListener!,
-    //   );
-    // }
-
-    // _betterPlayerController = BetterPlayerController.network(
-    //     'https://stream.mux.com/' + widget.video.playbackIds[0] + '.m3u8')
-    //   ..addListener(_videoPlayerListener!)
-    //   ..setVolume(1.0)
-    //   ..setLooping(true)
-    //   ..initialize()
-    //   ..play().then((_) {
-    //     // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-    //     setState(() {});
-    //   });
   }
 
   Future _loadUserAndLog() async {
@@ -228,11 +116,6 @@ class _CarouselVideoPlayerState extends State<CarouselVideoPlayer> {
   @override
   void dispose() {
     super.dispose();
-    // if (_betterPlayerController != null) {
-    //   _betterPlayerController!.dispose();
-    // }
-
-    // _betterPlayerController = null;
   }
 
   @override
