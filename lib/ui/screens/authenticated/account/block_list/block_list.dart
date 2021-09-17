629728c1693af10d10a79f3334b1a8df7dbbacaf
Christopher Ishida <christopherishida@Christophers-MacBook-Pro.local>
2021-09-16 18:18:41 -0700

    Cleaning up.

--- lib/ui/screens/authenticated/account/block_list/block_list.dart
+++ lib/ui/screens/authenticated/account/block_list/block_list.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:superconnector_vm/core/models/superuser/superuser.dart';
-import 'package:superconnector_vm/core/utils/constants/colors.dart';
 import 'package:superconnector_vm/ui/components/bottom_sheet_tab.dart';
-import 'package:superconnector_vm/ui/components/buttons/chevron_back_button.dart';
 import 'package:superconnector_vm/ui/components/snack_bars/dark_snack_bar.dart';
 import 'package:superconnector_vm/ui/components/underline.dart';
 import 'package:superconnector_vm/ui/screens/authenticated/account/block_list/components/blocked_tile.dart';
@@ -75,75 +73,6 @@ class BlockList extends StatelessWidget {
             ),
             Underline(
               color: Colors.white.withOpacity(.2),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: superuser.blockedUsers.keys.length,
-                itemBuilder: (context, index) {
-                  return BlockedTile(
-                    superuserId: superuser.blockedUsers.keys.toList()[index],
-                    unblock: () async {
-                      await superuser
-                          .unblock(superuser.blockedUsers.keys.toList()[index]);
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        DarkSnackBar.createSnackBar(
-                          text: 'You unblocked them.',
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: ConstantColors.OFF_WHITE,
-        brightness: Brightness.light,
-        elevation: 0.0,
-        toolbarHeight: 0.0,
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              color: ConstantColors.OFF_WHITE,
-              height: 55.0,
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: ChevronBackButton(
-                        padding: const EdgeInsets.only(
-                          left: 16.0,
-                          top: 17.0,
-                        ),
-                        color: ConstantColors.PRIMARY,
-                        onBack: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ),
-                  ),
-                  Text(
-                    'Block List',
-                    style: TextStyle(
-                      color: Colors.black.withOpacity(.87),
-                      fontSize: 18.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Spacer(),
-                ],
-              ),
             ),
             Expanded(
               child: ListView.builder(
--- lib/ui/screens/authenticated/account/block_list/components/blocked_tile.dart
+++ lib/ui/screens/authenticated/account/block_list/components/blocked_tile.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:superconnector_vm/core/models/superuser/superuser.dart';
 import 'package:superconnector_vm/core/services/superuser/superuser_service.dart';
-import 'package:superconnector_vm/core/utils/constants/colors.dart';
 import 'package:superconnector_vm/ui/components/images/superuser_image.dart';
-import 'package:superconnector_vm/ui/components/underline.dart';
 
 class BlockedTile extends StatefulWidget {
   BlockedTile({
@@ -105,62 +103,6 @@ class _BlockedTileState extends State<BlockedTile> {
               ),
             ],
           ),
-        ),
-        // Underline(
-        //   margin: const EdgeInsets.only(left: 69.0),
-        // ),
-        // Container(
-        //   height: 1.0,
-        //   // width: MediaQuery.of(context).size.width,
-        //   color: ConstantColors.CONTACTS_GROUP_BACKGROUND,
-        //   margin: const EdgeInsets.only(
-        //     left: 69.0,
-        //   ),
-        // ),
-      ],
-    );
-
-    return Column(
-      children: [
-        Padding(
-          padding: EdgeInsets.all(10.0),
-          child: ListTile(
-            leading: SuperuserImage(
-              radius: 21.0,
-              url: _superuser?.photoUrl,
-              bordered: false,
-            ),
-            // leading:
-            title: Text(
-              _superuser?.fullName ?? 'Loading...',
-              style: TextStyle(
-                color: ConstantColors.DARK_TEXT,
-                fontWeight: FontWeight.w600,
-                fontSize: 18.0,
-              ),
-            ),
-            subtitle: Text(
-              _superuser!.phoneNumber,
-              style: TextStyle(
-                color: ConstantColors.DARK_TEXT,
-                fontWeight: FontWeight.w400,
-                fontSize: 16.0,
-              ),
-            ),
-            trailing: GestureDetector(
-              onTap: () {
-                widget.unblock();
-              },
-              child: Image.asset(
-                'assets/images/authenticated/unblock-button.png',
-                height: 20.0,
-                width: 20.0,
-              ),
-            ),
-          ),
-        ),
-        Underline(
-          margin: const EdgeInsets.only(left: 85.0),
         ),
       ],
     );
